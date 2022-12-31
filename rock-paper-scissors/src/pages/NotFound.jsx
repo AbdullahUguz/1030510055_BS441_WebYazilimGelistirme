7e14f90f94e3d5ba90bf853276a8af9afe0710b0
Abdullah UĞUZ <abdullahuguz08@gmail.com>
2022-12-31 14:00:48 +0300

    last commit

--- rock-paper-scissors/src/pages/NotFound.jsx
+++ rock-paper-scissors/src/pages/NotFound.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function NotFound() {
   return (
     <div>
-        <h1 className='text-center'>NOT FOUND !!!</h1>
+        <h1 className='text-center'> NOT FOUND !!!</h1>
     </div>
   )
 }
